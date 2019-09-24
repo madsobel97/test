e37ead015b68ff7fdc61dab51229ba5002e42cb2
madsobel97 <55129348+madsobel97@users.noreply.github.com>
2019-09-24 08:36:57 +0200

    Hej far

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         String myUsername = "madsobel97";
-        String myPassword = "Password";
+        String myPassword = "Password123";
 
         if(!User.validateUsername(myUsername)) {
             System.out.println("Username doesn't fit the rules. Please check the User class.");
